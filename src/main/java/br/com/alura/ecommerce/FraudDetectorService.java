45098fbebb5ea0a86351dff7efd20b35a8766bd0
Rafael Monteiro Porto <rafael.monteiro.porto@outlook.com>
2022-03-18 08:20:18 -0300

    feat(Kafka Produtores, Consumidores e streams): Aula 04
    Atividades:
    - Serialização com GSON
    - Migrando o log
    - Deserialização customizada
    - Lidando com customizações

--- src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
+++ src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -3,18 +3,22 @@ package br.com.alura.ecommerce;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.util.Map;
 import java.util.Properties;
 
 public class FraudDetectorService {
 
     public static void main(String[] args) {
         var fraudService = new FraudDetectorService();
-        try(var service = new KafkaService(FraudDetectorService.class.getName(),"ECOMMERCE_NEW_ORDER", fraudService::parse)) {
+        try(var service = new KafkaService<Order>(FraudDetectorService.class.getName(),"ECOMMERCE_NEW_ORDER",
+                fraudService::parse,
+                Order.class,
+                Map.of())) {
             service.run();
         }
     }
 
-    private void parse(ConsumerRecord<String,String> record)
+    private void parse(ConsumerRecord<String,Order> record)
     {
         System.out.println("----------------------------------------");
         System.out.println("Processing new order, checking for fraud");
--- src/main/java/br/com/alura/ecommerce/KafkaDispatcher.java
+++ src/main/java/br/com/alura/ecommerce/KafkaDispatcher.java
@@ -10,14 +10,14 @@ import java.io.Closeable;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
-public class KafkaDispatcher implements Closeable {
-    private final KafkaProducer<String, String> producer;
+public class KafkaDispatcher<T> implements Closeable {
+    private final KafkaProducer<String, T> producer;
 
     public KafkaDispatcher() {
-        this.producer = new KafkaProducer<String, String>(properties());
+        this.producer = new KafkaProducer<String, T>(properties());
     }
 
-    void send(String topic, String key, String value) throws ExecutionException, InterruptedException {
+    void send(String topic, String key, T value) throws ExecutionException, InterruptedException {
 
         var record = new ProducerRecord(topic, key, value);
         Callback callback = (data, ex) -> {
@@ -35,7 +35,7 @@ public class KafkaDispatcher implements Closeable {
         var properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GsonSerializer.class.getName());
 
         return properties;
     }
